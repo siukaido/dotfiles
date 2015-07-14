9e6665260aa3425d8b5df0c727acd01b6154f455
siukaido <siukaido@gmail.com>
2015-07-14 19:47:42 +0900

    - update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,7 +22,7 @@
     auto-highlight-symbol
     color-theme
     foreign-regexp
-    ;elscreen
+    elscreen
     ;; 20
     apache-mode
     google-c-style
@@ -39,6 +39,8 @@
     ssh-config-mode
     yaml-mode
     web-mode
+    coffee-mode
+    typescript-mode
     ;; 30
     flyspell
     flycheck
@@ -65,22 +67,3 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
-
-(elscreen-start)
-(setq elscreen-prefix-key "\C-z")
-
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(defun web-mode-hook ()
-  "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset   4))
-(add-hook 'web-mode-hook 'web-mode-hook)
-(set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "white")
-(set-face-attribute 'web-mode-html-tag-face nil :foreground "LightSteelBlue")
-(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "color-222")
--- emacs.d/inits/40-elscreen.el
+++
@@ -1,6 +0,0 @@
-;(when (locate-library "elscreen")
-;  (setq elscreen-prefix-key "\C-z")
-;  (require 'elscreen))
-;(global-set-key "\M-}" 'elscreen-next)
-;(global-set-key "\M-{" 'elscreen-previous)
-;(define-key elscreen-map "\C-k" 'elscreen-kill-screen-and-buffers)
