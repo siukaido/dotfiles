264b11ae2dd38c54967cb0ad843e8d3f5465c799
siukaido <siukaido@gmail.com>
2015-07-14 19:40:59 +0900

    - update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,6 +22,7 @@
     auto-highlight-symbol
     color-theme
     foreign-regexp
+    ;elscreen
     ;; 20
     apache-mode
     google-c-style
@@ -64,3 +65,22 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
+
+(elscreen-start)
+(setq elscreen-prefix-key "\C-z")
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset   4))
+(add-hook 'web-mode-hook 'web-mode-hook)
+(set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "white")
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "LightSteelBlue")
+(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "color-222")
