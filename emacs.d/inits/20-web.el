ef6c6f835c13f3bc3c3b739a7e830e27edd63ec6
siukaido <siukaido@gmail.com>
2015-11-23 10:33:09 +0900

    - update

--- emacs.d/inits/20-web.el
+++ emacs.d/inits/20-web.el
@@ -6,6 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'". web-mode))
 (defun web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset   4))
