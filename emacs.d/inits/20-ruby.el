a5980f5a4dbb4e8cddb90cf3c083e7774a5678bd
siukaido <siukaido@gmail.com>
2017-02-08 20:06:59 +0900

    - update inits

--- emacs.d/inits/20-ruby.el
+++ emacs.d/inits/20-ruby.el
@@ -1,3 +1,8 @@
 ;; ruby-mode
 (require 'ruby-mode)
 (setq ruby-insert-encoding-magic-comment nil)
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (setq flycheck-checker 'ruby-rubocop)
+            (setq flycheck-ruby-rubocop-executable "~/.rbenv/shims/rubocop")
+            (flycheck-mode 1)))
