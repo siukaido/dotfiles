085e9ca8640d6645709b465835c77a60a0bbf877
siukaido <siukaido@gmail.com>
2019-05-17 16:29:04 +0900

    update

--- emacs.d/inits/30-eglot.el
+++ emacs.d/inits/30-eglot.el
@@ -1,10 +1,12 @@
 ;; eglot
 ;; M-.で定義ジャンプ、M-,でジャンプ先からもどる
-;; eglot はデフォルトの Language Server として go-langserver を使うので golsp に変更する
+;; eglot はデフォルトの Language Server として go-langserver を使うので gopls に変更する
 ;; 事前に go get -u golang.org/x/tools/cmd/gopls しておくこと
 (use-package eglot
   :config
+  (add-to-list 'eglot-server-programs '(go-mode . ("gopls")))
+  (add-hook 'go-mode-hook 'eglot-ensure)
+  ;; keybind change
   (define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions)
   (define-key eglot-mode-map (kbd "M-,") 'pop-tag-mark)
-  (add-to-list 'eglot-server-programs '(go-mode . ("gopls")))
-  (add-hook 'go-mode-hook 'eglot-ensure))
+)
--- emacs.d/inits/30-go.el
+++ emacs.d/inits/30-go.el
@@ -6,6 +6,12 @@
 (use-package go-mode
   :commands go-mode
   :defer t
+  ;; :init
+  ;; (add-hook 'go-mode-hook (lambda()
+  ;;                           (setq indent-tabs-mode nil)
+  ;;                           (setq c-basic-offset 4)
+  ;;                           (setq tab-width 4)
+  ;;                           ))
   :config
   (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save))
