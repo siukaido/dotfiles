6b4e8a0cea43a7c7eaf22f9f08717db6c60987af
siukaido <siukaido@gmail.com>
2020-07-31 20:29:19 +0900

    update web-mode & typescript-mode settings

--- emacs.d/inits/40-type-script.el
+++ emacs.d/inits/40-type-script.el
@@ -1,5 +1,4 @@
 (add-to-list 'auto-mode-alist '(".*\\.ts\\'" . typescript-mode))
-(add-to-list 'auto-mode-alist '(".*\\.tsx\\'" . typescript-mode))
 (add-hook 'typescript-mode-hook
           (lambda ()
             (setq typescript-indent-level 2)))
--- emacs.d/inits/40-web.el
+++ emacs.d/inits/40-web.el
@@ -2,6 +2,9 @@
   :init
   (add-hook 'web-mode-hook (lambda()
                              (setq indent-tabs-mode nil)
+                             (setq web-mode-markup-indent-offset 2)
+                             (setq web-mode-css-indent-offset 2)
+                             (setq web-mode-code-indent-offset 2)
                              (setq tab-width 2)
                              ))
   )
@@ -13,10 +16,10 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'". web-mode))
+(add-to-list 'auto-mode-alist '(".*\\.tsx\\'" . web-mode))
 (defun web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset   2))
 (add-hook 'web-mode-hook 'web-mode-hook)
 (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "white")
-(set-face-attribute 'web-mode-html-tag-face nil :foreground "LightSteelBlue")
-(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "color-222")
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "NavajoWhite")
