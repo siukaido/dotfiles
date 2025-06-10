c647327574f056d0b51bff35de078a8a0296cd35
siukaido <siukaido@gmail.com>
2025-06-10 10:45:06 +0900

    fix a bug

--- emacs.d/inits/00-melpa.el
+++ emacs.d/inits/00-melpa.el
@@ -1,7 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t
-             '("org" . "https://orgmode.org/elpa/") t
-             '("gnu" . "https://elpa.gnu.org/packages/") t
-             )
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+             '("gnu" . "https://elpa.gnu.org/packages/") t)
 (package-initialize)
